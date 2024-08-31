f607f2e589c5f4b97ee45412b8efe57b582796f1
qoala <qoala@qoalas-MacBook-Air.local>
2024-08-31 11:58:45 +0530

    fix

--- src/components/dialog/DialogForm.tsx
+++ src/components/dialog/DialogForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
 
--- src/components/home/InsuranceOptions.tsx
+++ src/components/home/InsuranceOptions.tsx
@@ -14,7 +14,6 @@ interface Props {
 const InsuranceOption = ({
   icon,
   text,
-  navigate,
   action,
 }: {
   icon: any;
